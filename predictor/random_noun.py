acec7d07b43cdbe1861025a5da3e0c96a7cb634f
Ben Barantschik <bbarantschik@gmail.com>
2019-04-10 21:55:37 -0700

    finished basic interface for looking through tokens

--- predictor/random_noun.py
+++ predictor/random_noun.py
@@ -5,13 +5,15 @@ GET_TOKEN_POS = "SELECT pos FROM tokens WHERE token_id=%(token_id)s"
 # Initial randomized data for training
 def get_random_noun_list(cursor, token_ids, n = 64):
     token_ids = list(token_ids)
-    candidates = []
+    candidates = set()
     while len(candidates) < n:
         token_id = random.choice(token_ids)
+        if token_id in candidates:
+            continue
         cursor.execute(GET_TOKEN_POS, {
             'token_id': token_id
         })
         pos = cursor.fetchall()[0][0]
         if pos == 'NN':
-            candidates.append(token_id)
-    return sorted(candidates)
+            candidates.add(token_id)
+    return sorted(list(candidates))
