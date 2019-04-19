284e89e0979c4dfe4decea2894624231e4f42574
Ben Barantschik <bbarantschik@gmail.com>
2019-04-19 13:39:01 -0700

    Will now return all occurrences of a phrase, not just those returned by regex.

--- predictor/random_noun.py
+++ predictor/random_noun.py
@@ -7,7 +7,7 @@ GET_TOKEN_INFO = "SELECT token_id, word, pos FROM tokens WHERE ssid=%(ssid)s ORD
 # see http://bdewilde.github.io/blog/2014/09/23/intro-to-automatic-keyphrase-extraction/
 # for some more ideas?
 
-def get_random_noun_list(cursor, ssid, n = 64, max_failures = 16):
+def get_random_noun_list(cursor, ssid, n = 16, max_failures = 16):
     cursor.execute(GET_TOKEN_INFO, {
         'ssid': ssid
     })
@@ -26,11 +26,25 @@ def get_random_noun_list(cursor, ssid, n = 64, max_failures = 16):
     
     phrases_to_ids = {}
     for span in nn_locs:
+        size = span[1] - span[0]
         info_for_span = info[span[0]:span[1]]
-        phrase = ' '.join([word for _, word, _ in info_for_span])
+        words_spanned = [word for _, word, _ in info_for_span]
+        phrase = ' '.join(words_spanned)
         if phrase not in phrases_to_ids:
+            # We can't just use the matches found by regex since this will skip
+            # noun phrases found within noun phrases
             phrases_to_ids[phrase] = []
-        phrases_to_ids[phrase].append([token_id for token_id, _, _ in info_for_span])
+            for info_i in range(len(info)):
+                if info_i + size - 1 >= len(info):
+                    break
+                match = True
+                possible_words_spanned = [word for _, word, _ in info[info_i:info_i + size]]
+                for word_a, word_b in zip(words_spanned, possible_words_spanned):
+                    if word_a != word_b:
+                        match = False
+                        break
+                if match:
+                    phrases_to_ids[phrase].append([token_id for token_id, _, _ in info[info_i:info_i + size]])
         
     # We filter out any phrases that only occur once
     phrases_to_ids = {phrase:ids for phrase,ids in phrases_to_ids.items() if len(ids) > 1}
